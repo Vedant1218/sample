9c02c16a62b072aecea459477e0d4e2a888e3b0c
Vedant1218 <43043206+Vedant1218@users.noreply.github.com>
2018-10-18 13:59:39 -0400

    added prompts

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,18 @@ function test1(){
 	var fName = prompt("Hey what's your name?");	
 	var mycolour = prompt("pick a colour, " + fName)
 
+	if(mycolour == "red"){
 
 
-
+		console.log("ha. You like red");
+	}
+	else if(mycolour == "green"){
+		console.log("ha. Green hes alright.");
+	}
+	else if(mycolour == "yellow"){
+		console.log("get him something yellow");
+	}
+	else {
+		console.log("thats not a colour")
+	}
 }
